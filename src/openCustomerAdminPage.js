64150e92125fbaddc5772db329e157d2fbae4286
TimTim <ticlark@squarespace.com>
2022-08-19 11:31:54 -0400

    Add admin page bookmarklet

---
+++ src/openCustomerAdminPage.js
@@ -0,0 +1,15 @@
+
+/**
+ * Copy-pastable bookmarklet
+ */
+javascript:window.open('https://customersupport.' + window.location.host.split('.').slice(1).join('.') + '/squarespace-admin/support/site/' + window.location.host.split('.')[0], '_blank')
+
+/**
+ * Open any customer page in the admin support tool
+ */
+
+javascript:window.open(
+  'https://customersupport.' + 
+  window.location.host.split('.').slice(1).join('.') + 
+  '/squarespace-admin/support/site/' + 
+  window.location.host.split('.')[0], '_blank')
